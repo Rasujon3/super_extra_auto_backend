3d70510db61f92b40d440692fdb3d1e2db101baf
Sujon Office <>
2025-06-29 16:44:05 +0600

    first commit

---
+++ routes/api.php
@@ -0,0 +1,27 @@
+<?php
+
+use App\Http\Controllers\Api\BranchController;
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Route;
+use Illuminate\Support\Facades\Artisan;
+
+/*
+|--------------------------------------------------------------------------
+| API Routes
+|--------------------------------------------------------------------------
+|
+| Here is where you can register API routes for your application. These
+| routes are loaded by the RouteServiceProvider within a group which
+| is assigned the "api" middleware group. Enjoy building your API!
+|
+*/
+
+Route::get('/branches', [BranchController::class, 'index']);
+
+Route::get('/clear-cache', function () {
+    Artisan::call('config:clear');
+    Artisan::call('config:cache');
+    Artisan::call('route:clear');
+
+    return "Configuration, route, and cache cleared successfully!";
+});
